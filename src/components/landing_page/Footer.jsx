6b85fda683ac81843f10c518509fad7312deba8a
damir <bajrami.damir@gmail.com>
2023-03-18 14:32:16 +0100

    fixed up the footer

--- src/components/landing_page/Footer.jsx
+++ src/components/landing_page/Footer.jsx
@@ -2,22 +2,67 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='min-w-screen py-16 flex justify-center bg-emerald-400'>
-        <div className='w-96'>
-            <div className='grid grid-cols-3 gap-3 justify-items-center mb-3'>
-                <a href="#"><img className="h-12 invert" src="/assets/socials-icons/github.svg" alt="" /></a>
-                <a href="#"><img className="h-12 invert" src="/assets/socials-icons/instagram.svg" alt="" /></a>
-                <a href="#"><img className="h-12 invert" src="/assets/socials-icons/linkedin.svg" alt="" /></a>
-            </div>
-            <div className='grid grid-flow-row-dense grid-cols-6 grid-rows-2 justify-items-center text-slate-200'>
-                <a className='col-span-2 hover:text-white' href="#">Info</a>
-                <a className='col-span-2 hover:text-white' href="#">Support</a>
-                <a className='col-span-2 hover:text-white' href="#">Marketing</a>
-                <a className='col-span-3 hover:text-white' href="#">Terms of Use</a>
-                <a className='col-span-3 hover:text-white' href="#">Privacy Policy</a>
-            </div>
+    <footer className='bg-emerald-400'>
+      <div className='mx-auto w-full container px-80'>
+        <div className='grid grid-cols-3 px-32 py-8 text-slate-200'>
+          <div className='text-center '>
+            <button>
+              <a href="#">
+                <img src="assets/socials-icons/github.svg" className='w-12 mb-4 invert mx-auto' />
+              </a>
+            </button>
+            <ul>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Projects</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Contact</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Marketing</a>
+              </li>
+            </ul>
+          </div>
+          <div className='text-center '>
+            <button>
+              <a href="#">
+                <img src="assets/socials-icons/instagram.svg" className='w-12 mb-4 invert mx-auto' />
+              </a>
+            </button>
+            <ul>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Brand Center</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Blog</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Careers</a>
+              </li>
+            </ul>
+          </div>
+          <div className='text-center '>
+            <button>
+              <a href="#">
+                <img src="assets/socials-icons/linkedin.svg" className='w-12 mb-4 invert mx-auto' />
+              </a>
+            </button>
+            <ul>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Info</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Support</a>
+              </li>
+              <li className='mb-4'>
+                <a href="" className='hover:text-white'>Marketing</a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
+      </div>
+    </footer>
+
   )
 }
 
