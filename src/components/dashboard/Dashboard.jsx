15dc175edca0b72b21ee1fb0c460dfc5d93411d3
amar <cajdric.a@gmail.com>
2023-03-18 14:39:11 +0100

    Made temparature card

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import LightCard from './LightCard'
+import TempCard from './TempCard'
 
 
 const Dashboard = () => {
   return (
     <div className='bg-gray-200 text-slate-800 h-screen'>
-    <header className=' text-center text-2xl'>
-        <p>gimmie some head hoe </p> </header>
-        <div><p>sugoma balls</p></div>
+        <header className='text-white bg-emerald-400 mb-16 py-5 text-center text-5xl font-semibold tracking-widest'>
+            <h1>DASHBOARD</h1>
+        </header>
         <div className='mx-auto w-4/5 text-center grid grid-cols-4 gap-4'>
             <LightCard image1={"https://cdn1.vectorstock.com/i/1000x1000/30/40/light-bulb-icon-simple-style-vector-8633040.jpg"} image2={"https://as1.ftcdn.net/v2/jpg/02/00/80/28/1000_F_200802852_u4Vs7EsYmMRNokt1hDOIdfCIzRbE3muo.jpg"}/>
-            <div >02</div>
+            <div ><TempCard /></div>
             <div >03</div>
             <div >04</div>
 
---
+++ src/components/dashboard/TempCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TempCard = () => {
+  return (
+    <div>
+        <div className='shadow-lg p-4 rounded-lg grid grid-cols-2'>
+            <img src="/assets/temp.svg" alt="" />
+            <div>
+                <h3 className='text-2xl text-white font-light '>Temperature</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TempCard
