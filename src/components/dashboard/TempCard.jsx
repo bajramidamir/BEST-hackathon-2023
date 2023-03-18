5bb35507a8bbcc04e9cb36d6634cf49319dbb35e
amar <cajdric.a@gmail.com>
2023-03-18 16:45:21 +0100

    temp change buttons work now

--- src/components/dashboard/TempCard.jsx
+++ src/components/dashboard/TempCard.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TempCard = () => {
+
+  const [temperatureValue, setTemperatureValue] = useState(15);
+
+
+  const handleTempChange = ( operation ) => {
+    if (operation === "increment")  {
+      setTemperatureValue(temperatureValue + 1)
+    } else if (operation === "decrement") {
+      setTemperatureValue(temperatureValue - 1)
+    }
+  }
+
   return (
         <div className='bg-slate-50 shadow-lg p-4 rounded-lg grid grid-cols-2 items-center'>
             <img className="px-2" src="/assets/temp.svg" alt="" />
             <div>
                 <h3 className='text-xl font-extrabold overflow-clip'>Temperature</h3>
-                <button><img className='w-14' src="/assets/up.svg" alt="" /></button>
-                <p className='text-5xl mb-2'>55°C</p>
-                <button><img className='w-14' src="/assets/down.svg" alt="" /></button>
+                <button onClick={() => handleTempChange("increment")}>
+                  <img className='w-14' src="/assets/up.svg" alt="" />
+                </button>
+                <p className='text-5xl mb-2'>{temperatureValue}°C</p>
+                <button onClick={() => handleTempChange("decrement")}>
+                  <img className='w-14' src="/assets/down.svg" alt="" />
+                </button>
             </div>
         </div>
   )
