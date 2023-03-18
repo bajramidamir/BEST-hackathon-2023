e1cc159a0240c1c75fe5f123a919dc6ba074d0e9
damir <bajrami.damir@gmail.com>
2023-03-18 12:35:05 +0100

    broken up LandingPage into more components

--- src/components/landing_page/LandingPage.jsx
+++ src/components/landing_page/LandingPage.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Footer from "./Footer"
+import LandingPageHeader from './LandingPageHeader';
 
 const LandingPage = () => {
   return (
     <div>
-        <div className='mb-6'>
-            <img src="assets/waves.svg"  />
-        </div>
-       
+        <LandingPageHeader />
         <Hero />
         <Footer />
-
     </div>
   )
 }
---
+++ src/components/landing_page/LandingPageHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPageHeader = () => {
+  return (
+    <div className='mb-6'>
+            <img src="assets/waves.svg"  />
+    </div>
+  )
+}
+
+export default LandingPageHeader
