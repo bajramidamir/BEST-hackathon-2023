f6ecc5be6558cb062dc2fe0272ba3970d13fbc9f
amar <cajdric.a@gmail.com>
2023-03-18 15:47:02 +0100

    Making temperature card

--- src/components/dashboard/TempCard.jsx
+++ src/components/dashboard/TempCard.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const TempCard = () => {
   return (
     <div>
-        <div className='shadow-lg p-4 rounded-lg grid grid-cols-2'>
-            <img src="/assets/temp.svg" alt="" />
+        <div className='bg-slate-50 shadow-lg p-4 rounded-lg grid grid-cols-2 items-center'>
+            <img className="px-2" src="/assets/temp.svg" alt="" />
             <div>
-                <h3 className='text-2xl text-white font-light '>Temperature</h3>
-                <p className='text-5xl'></p>
+                <h3 className='text-3xl font-light'>Temperature</h3>
+                <button><img className='w-14' src="/assets/up.svg" alt="" /></button>
+                <p className='text-5xl mb-2'>55°C</p>
+                <button><img className='w-14' src="/assets/down.svg" alt="" /></button>
             </div>
         </div>
     </div>
